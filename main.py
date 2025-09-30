906d04b461e68e174daadaecd93f352da30bd52c
barion <murilobarion.p@gmail.com>
2025-09-29 22:19:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ locais = {
 }
 
 for arquivo in lista_arquivos:
-    # 01. ARquivo.pdf
+    
     nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
     for pasta in locais:
         if extensao in locais[pasta]:
             if not os.path.exists(f"{caminho}/{pasta}"):
                 os.mkdir(f"{caminho}/{pasta}")
+
             os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
